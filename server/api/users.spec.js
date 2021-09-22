ef45e1b8de5191223a0d708b33d8202952685633
William Leet <williamleet@Chrisophers-MBP.lan>
2021-09-22 08:39:05 -0700

    Merge conflicts resolved, I hope

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -20,6 +20,38 @@ describe('User routes', () => {
 
         expect(res.body).to.be.an("array");
         expect(res.body.length).to.equal(3);
-    })
-  }) 
-}) 
+      });
+    });
+  
+    describe("/api/users/:userId", () => {
+      it("GET /api/users/:userId", async () => {
+        const user1 = await request(app).get("/api/users/1").expect(200);
+        const user2 = await request(app).get("/api/users/2").expect(200);
+  
+        expect(user1.body).to.be.an("object");
+        expect(user2.body).to.be.an("object");
+  
+        /* Because of promise.all we don't know who is who */
+        try {
+          expect(user1.body.username).to.equal("cody");
+          expect(user2.body.username).to.equal("murphy");
+  
+          expect(user1.body.email).to.equal("cody@the.best");
+          expect(user2.body.email).to.equal("murphy@the.best");
+        } catch (error) {
+          expect(user1.body.username).to.equal("murphy");
+          expect(user2.body.username).to.equal("cody");
+  
+          expect(user2.body.email).to.equal("cody@the.best");
+          expect(user1.body.email).to.equal("murphy@the.best");
+        }
+  
+        /* checking uf it's hashing */
+        expect(user1.body.password).to.not.equal("123");
+        expect(user2.body.password).to.not.equal("123");
+      });
+    });
+  
+    // end describe('/api/users')
+  }); // end describe('User routes')
+
