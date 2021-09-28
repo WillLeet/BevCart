0a60ba8a4f40f688e7498c485f665daa732b34bd
Matt Stern <mestern@smu.edu>
2021-09-28 17:19:51 -0400

    styling and front page fun

--- client/components/Home.js
+++ client/components/Home.js
@@ -9,9 +9,10 @@ export const Home = props => {
   const {username} = props
 
   return (
-    <div>
-      <h3>Welcome, {username}</h3>
-      <Link to="/products">SEE ALL PRODUCTS</Link>
+    <div id="homepage">
+      <h1>Welcome{username?(' Back '+username + '!'):('!')}</h1>
+      <img id="home-image" src='https://images.unsplash.com/photo-1527960471264-932f39eb5846?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80'></img>
+      <Link to="/products"><button>SEE ALL PRODUCTS</button></Link>
     </div>
   )
 }
