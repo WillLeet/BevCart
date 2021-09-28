d20bc0b5805081fa8be6fc93c28b471f7caa4282
emil <poolsar.42@gmail.com>
2021-09-29 00:35:52 +0300

    fix: fixed bug with adding product to cart

--- client/store/order.js
+++ client/store/order.js
@@ -14,49 +14,49 @@ const setOrder = (order) => {
 };
 
 const setCurrentOrder = (order) => {
-    return {
-      type: SET_CURRENT_ORDER,
-      order,
-    };
+  return {
+    type: SET_CURRENT_ORDER,
+    order,
   };
+};
 
-  const setOrderHistory = (orders) => {
-    return {
-      type: SET_ORDER_HISTORY,
-      orders,
-    };
+const setOrderHistory = (orders) => {
+  return {
+    type: SET_ORDER_HISTORY,
+    orders,
   };
+};
 
 const sentOrder = (orders) => {
-    return {
-        type: SEND_ORDER,
-        orders
-    }
-}
+  return {
+    type: SEND_ORDER,
+    orders,
+  };
+};
 
 const initializedOrder = (order) => {
-    return {
-        type: INITIALIZE_ORDER,
-        order
-    }
-}
-
-export const fetchOrder = (userId,orderId) => {
-    return async (dispatch) => {
-      try {
-        const { data } = await axios.get(`/api/order/${userId}/${orderId}`);
-        dispatch(setOrder(data));
-      } catch (err) {
-        console.error(err);
-      }
-    };
+  return {
+    type: INITIALIZE_ORDER,
+    order,
   };
+};
+
+export const fetchOrder = (userId, orderId) => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`/api/order/${userId}/${orderId}`);
+      dispatch(setOrder(data));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+};
 
 export const fetchCurrentOrder = (userId) => {
   return async (dispatch) => {
     try {
       const { data } = await axios.get(`/api/order/${userId}`);
-      console.log("In the thunk, data is",data);
+      console.log("In the thunk, data is", data);
       dispatch(setCurrentOrder(data));
     } catch (err) {
       console.error(err);
@@ -65,48 +65,53 @@ export const fetchCurrentOrder = (userId) => {
 };
 
 export const fetchOrderHistory = (userId) => {
-    return async (dispatch) => {
-      try {
-        const { data } = await axios.get(`/api/order/${userId}/history`);
-        dispatch(setOrderHistory(data));
-      } catch (err) {
-        console.error(err);
-      }
-    };
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`/api/order/${userId}/history`);
+      dispatch(setOrderHistory(data));
+    } catch (err) {
+      console.error(err);
+    }
   };
+};
 
 export const sendOrder = (userId) => {
   return async (dispatch) => {
     const { data: oldCurrent } = await axios.post(`/api/order/${userId}`);
-    const {data: newCurrent } = await axios.get(`/api/order/${userId}`);
-    console.log("oldCurrent is ",oldCurrent);
-    console.log("newCurrent is ",newCurrent);
-    dispatch(sentOrder({oldCurrent, newCurrent}))
+    const { data: newCurrent } = await axios.get(`/api/order/${userId}`);
+    console.log("oldCurrent is ", oldCurrent);
+    console.log("newCurrent is ", newCurrent);
+    dispatch(sentOrder({ oldCurrent, newCurrent }));
   };
 };
 
 export const initializeOrder = (userId) => {
-    return async (dispatch) => {
-      const { data: newOrder } = await axios.post(`/api/order/${userId}/new`);
-      dispatch(initializedOrder(newOrder));
-    };
+  return async (dispatch) => {
+    const { data: newOrder } = await axios.post(`/api/order/${userId}/new`);
+    dispatch(initializedOrder(newOrder));
   };
+};
 
 export default function orderReducer(state = [], action) {
   switch (action.type) {
     case SET_ORDER:
-        return [action.order];
+      return [action.order];
     case SET_ORDER_HISTORY:
-        return action.orders;
+      return action.orders;
     case SET_CURRENT_ORDER:
-        return [action.order];
+      return [action.order];
+    case INITIALIZE_ORDER:
+      return [action.order];
     case SEND_ORDER:
-        console.log("State is ",state);
-        const tomap = [...state, action.orders.newCurrent]
-        console.log("Tomap is ",tomap);
-        return tomap.map(order => (order.id === action.orders.oldCurrent.id ? action.orders.oldCurrent : order))
+      console.log("State is ", state);
+      const tomap = [...state, action.orders.newCurrent];
+      console.log("Tomap is ", tomap);
+      return tomap.map((order) =>
+        order.id === action.orders.oldCurrent.id
+          ? action.orders.oldCurrent
+          : order
+      );
     default:
       return state;
   }
 }
-
